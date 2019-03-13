a4b40780e01da570c931ea3ecfa568ae866e8c86
Andrew-M-C <laplacezhang@126.com>
2019-03-13 17:20:11 +0800

    Test gorm

--- log/log.go
+++ log/log.go
@@ -3,11 +3,65 @@ package log
 import (
 	"fmt"
 	"time"
+	"runtime"
+	"strings"
+	"path/filepath"
+	"strconv"
 )
 
+func getCallerInfo(invoke_level int) (fileName string, line int, funcName string) {
+	funcName = "FILE"
+	line = -1
+	fileName = "FUNC"
+
+	if invoke_level <= 0 {
+		invoke_level = 2
+	} else {
+		invoke_level += 1
+	}
+
+	pc, file_name, line, ok := runtime.Caller(invoke_level)
+	if ok {
+		fileName = filepath.Base(file_name)
+		func_name := runtime.FuncForPC(pc).Name()
+		func_name = filepath.Ext(func_name)
+		funcName = strings.TrimPrefix(func_name, ".")
+	}
+	// fmt.Println(reflect.TypeOf(pc), reflect.ValueOf(pc))
+	return
+}
+
+func getTimeStr() string {
+	return time.Now().Local().Format("2006-01-02 15:04:05.000000")
+}
+
 func Debug(format string, v ...interface{}) {
-	now := time.Now()
-	datetime := now.Local().Format("2006-01-02 15:04:05.000000")
-	fmt.Printf(datetime + " - DEBUG - " + format + "\n", v...)
+	datetime := getTimeStr()
+	file, line, function := getCallerInfo(0);
+	fmt.Printf(datetime + " - DEBUG - " + file + ", " + strconv.Itoa(line) + ", " + function + "() - " + format + "\n", v...)
+	return
+}
+
+
+func Info(format string, v ...interface{}) {
+	datetime := getTimeStr()
+	file, line, function := getCallerInfo(0);
+	fmt.Printf(datetime + " - INFO  - " + file + ", " + strconv.Itoa(line) + ", " + function + "() - " + format + "\n", v...)
+	return
+}
+
+
+func Warn(format string, v ...interface{}) {
+	datetime := getTimeStr()
+	file, line, function := getCallerInfo(0);
+	fmt.Printf(datetime + " - WARN  - " + file + ", " + strconv.Itoa(line) + ", " + function + "() - " + format + "\n", v...)
+	return
+}
+
+
+func Error(format string, v ...interface{}) {
+	datetime := getTimeStr()
+	file, line, function := getCallerInfo(0);
+	fmt.Printf(datetime + " - ERROR - " + file + ", " + strconv.Itoa(line) + ", " + function + "() - " + format + "\n", v...)
 	return
 }
--- main.go
+++ main.go
@@ -1,9 +1,52 @@
 package main
 
 import (
+	_ "time"
+	"reflect"
 	"./log"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+type TestTable struct {
+	ID			uint `gorm:"primary_key"`
+}
+
+func (TestTable) TableName() string {
+	return "t_test"
+}
+
+func checkTableExist(db *gorm.DB) {
+	tablePtr := &TestTable{}
+	log.Debug("Type: %s", reflect.TypeOf(*tablePtr))
+
+	if db.HasTable(tablePtr) {
+		log.Info("Has table")
+		return
+	} else {
+		log.Info("Does not have table")
+	}
+	// does not have table
+	// db.Set("ENGINE=InnoDB", "DEFAULT CHARSET=utf8").CreateTable(tablePtr)
+	// log.Info("Create table")
+}
+
+func testReflect(v interface{}) {
+	log.Debug("Type of v: %s", reflect.TypeOf(v));
+}
+
 func main() {
-	log.Debug("Hello, Go!")
+	log.Debug("Hello, gorm!")
+
+	db, err := gorm.Open("mysql", "tars:tars2015@tcp(10.0.4.11:3306)/db_gorm?charset-utf8&parseTime=True&loc=Local")
+	defer db.Close()
+	if err != nil {
+		log.Error("Error opening DB: %s", err.Error())
+	} else {
+		log.Info("Open DB OK!")
+	}
+
+	checkTableExist(db)
+	testReflect(db)
+	return
 }
